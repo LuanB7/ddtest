9694db377b250896fcc938beb10f12adb42a85a6
Luan Bonholli <78662457+LuanB7@users.noreply.github.com>
2022-09-18 00:56:08 -0300

    Add files via upload

--- JS/global.js
+++ JS/global.js
@@ -1,14 +1,18 @@
 function getCoordenada(event) {
     let sub = document.querySelector(".sub-menu");
+    sub.style.transition = '0s';
+    
 
     function transformSubPos(event) {
         sub.style.top = (event.pageY - 28.5) + "px";
+        sub.style.transition = '0s';
         console.log(event.pageY);
         
     }
 
     function transformSubPosTouch(event) {
         sub.style.top = (event.touches[0].pageY - 28.5) + "px";
+        sub.style.transition = '0s';
         console.log("Touch: " + event.touches[0].pageY);
         
     }
@@ -21,9 +25,57 @@ function getCoordenada(event) {
     sub.addEventListener("mouseup", function(event){
         //sub.style.left = event.pageX + "px";
         
-
+        sub.style.transiton = '0.4s';
         sub.removeEventListener("mousemove", transformSubPos);
+        sub.removeEventListener("touchstart", transformSubPosTouch);
+
+        if (event.pageY > 700) {
+            sub.style.backgroundColor = 'red';
+        }
+        else {
+            sub.style.backgroundColor = 'blue';
+        }
+        
         console.log('a');
+
+    });
+
+
+
+    sub.addEventListener("touchend", function(event){
+        //sub.style.left = event.pageX + "px";
+        
+        sub.style.transition = '0.4s';
+        sub.removeEventListener("mousemove", transformSubPos);
+        sub.removeEventListener("touchstart", transformSubPosTouch);
+
+       
+        if (sub.style.top > (window.screen.availHeight / 3) * 2 + "px") { 
+            
+            console.log(sub.style.top);
+            console.log(window.screen.availHeight / 2);
+            sub.style.backgroundColor = 'red';
+            console.log('maior');
+
+            sub.style.top = window.screen.availHeight - 57 + "px";
+        }
+        else {
+            
+            console.log(sub.style.top);
+            sub.style.backgroundColor = 'blue';
+            console.log(window.screen.availHeight / 2);
+            console.log('menor');
+            sub.style.top = '57px';
+        }
+
+        /*if (event.touches[0].pageY > 700) {
+            
+        }
+        else {
+            
+        }*/
+        
+       
 
     });
 
