d53c76406d749247ff8c5e38bd3e0e41f22797d0
Luan Bonholli <78662457+LuanB7@users.noreply.github.com>
2022-09-18 19:06:16 -0300

    Add files via upload

--- JS/global.js
+++ JS/global.js
@@ -21,84 +21,52 @@ function getCoordenada(event) {
     
     sub.addEventListener("mousemove", transformSubPos);
 
-    sub.addEventListener("touchmove", transformSubPosTouch)
-
-    sub.addEventListener("mouseup", function(event){
-        //sub.style.left = event.pageX + "px";
-        event.preventDefault();
-        sub.style.transition = '0.4s';
-        sub.removeEventListener("mousemove", transformSubPos);
-        sub.removeEventListener("touchmove", transformSubPosTouch);
-        sub.removeEventListener("touchstart", transformSubPosTouch);
-
-        if (sub.style.top > `${window.screen.availHeight / 2}px`) { 
-            
-            console.log("top: " + sub.style.top);
-            console.log(window.screen.availHeight / 2);
-            sub.style.backgroundColor = 'red';
-            console.log('maior');
-
-            setTimeout(()=>{
-                sub.style.top = "calc(100% - 57px)";
-            }, 10);
-            
-        }
-        else {
-            console.log("top: " + sub.style.top);
-            sub.style.backgroundColor = 'blue';
-            console.log(window.screen.availHeight / 2);
-            console.log('menor');
-            setTimeout(()=>{
-                sub.style.top = "57px";
-            }, 10);
-        }
-
-    });
+    sub.addEventListener("touchmove", transformSubPosTouch);
 
 
+    function mouseUp(event) {
+          //sub.style.left = event.pageX + "px";
+          event.preventDefault();
+          sub.style.transition = '0.4s';
+          sub.removeEventListener("mousemove", transformSubPos);
+          sub.removeEventListener("touchmove", transformSubPosTouch);
+          sub.removeEventListener("touchstart", transformSubPosTouch);
+          sub.removeEventListener("mouseup", mouseUp);
+          sub.removeEventListener("touchend", mouseUp);
+          
+          
+  
+          if (sub.style.top > `${window.screen.availHeight / 2}px`) { 
+              
+              console.log("top: " + sub.style.top);
+              console.log(window.screen.availHeight / 2);
+              //sub.style.backgroundColor = 'red';
+              console.log('maior');
+  
+              setTimeout(()=>{
+                    sub.style.top = "calc(100% - 57px)";
+              }, 10);
+              
+          }
+          else {
+              console.log("top: " + sub.style.top);
+              //sub.style.backgroundColor = 'blue';
+              console.log(window.screen.availHeight / 2);
+              console.log('menor');
+              setTimeout(()=>{
+                    sub.style.top = "57px";
+                    let smca = document.querySelectorAll(".sm-content-a");
+                    for (i=0; i < smca.length; i++) {
+                        smca[i].style.opacity = 1;
+                        smca[i].style.transform = "scale(1)";
+                    }
+              }, 10);
+          }
+  
+    }
 
-    sub.addEventListener("touchend", function(event){
-        //sub.style.left = event.pageX + "px";
-        
-        sub.style.transition = '0.4s';
-        sub.removeEventListener("mousemove", transformSubPos);
-        sub.removeEventListener("touchmove", transformSubPosTouch);
-        sub.removeEventListener("touchstart", transformSubPosTouch);
-        event.preventDefault();
-
-       
-        if (sub.style.top > `${window.screen.availHeight / 2}px`) { 
-            
-            console.log("top: " + sub.style.top);
-            console.log(window.screen.availHeight / 2);
-            sub.style.backgroundColor = 'red';
-            console.log('maior');
-
-            setTimeout(()=>{
-                sub.style.top = "calc(100% - 57px)";
-            }, 10);
-            
-        }
-        else {
-            console.log("top: " + sub.style.top);
-            sub.style.backgroundColor = 'blue';
-            console.log(window.screen.availHeight / 2);
-            console.log('menor');
-            setTimeout(()=>{
-                sub.style.top = "57px";
-            }, 10);
-        }
-
-        /*if (event.touches[0].pageY > 700) {
-            
-        }
-        else {
-            
-        }*/
-        
-       
-
-    });
+    sub.addEventListener("mouseup", mouseUp);
+    sub.addEventListener("touchend", mouseUp);
 
     
 
